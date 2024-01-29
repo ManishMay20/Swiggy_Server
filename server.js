835b361f52ca3cdd2205f6894e383a9a5ce0f6b1
ManishMay20 <manishpatidar73289@gmail.com>
2024-01-29 22:59:52 +0530

    add new collection route

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const fetch = require('cross-fetch');
+const express = require("express");
+const cors = require("cors");
+const fetch = require("cross-fetch");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,74 +8,118 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 // For Restaurant API
-app.get('/api/restaurants', async (req, res) => {
-    const { lat, lng, page_type } = req.query;
-    console.log(req.query);
+app.get("/api/restaurants", async (req, res) => {
+  const { lat, lng, page_type } = req.query;
+  console.log(req.query);
 
-    const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=${page_type}`;
+  const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&page_type=${page_type}`;
 
-    await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-
-        }
+  await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            res.json(data);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send('An error occurred');
-        });
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
 });
-
 
 // For Menu API
-app.get('/api/menu', async (req, res) => {
-    const { 'page-type': page_type, 'complete-menu': complete_menu, lat, lng, submitAction, restaurantId } = req.query;
-    console.log(req.query);
+app.get("/api/menu", async (req, res) => {
+  const {
+    "page-type": page_type,
+    "complete-menu": complete_menu,
+    lat,
+    lng,
+    submitAction,
+    restaurantId,
+  } = req.query;
+  console.log(req.query);
 
-    const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
+  const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
 
-
-    await fetch(url, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-
-        }
+  await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
     })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            res.json(data);
-        })
-        .catch(error => {
-            console.error(error);
-            res.status(500).send('An error occurred');
-        });
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
 });
 
+// For collection API
+app.get("/api/restaurants/list", async (req, res) => {
+  const {
+    "page-type": page_type,
+    "complete-menu": complete_menu,
+    lat,
+    lng,
+    type,
+    collection,
+  } = req.query;
+  console.log(req.query);
 
-app.get('/', (req, res) => {
-    res.json({ "test": "Welcome to Swiggy api!" });
-})
+  //   const url = `https://www.swiggy.com/dapi/menu/pl?page-type=${page_type}&complete-menu=${complete_menu}&lat=${lat}&lng=${lng}&submitAction=${submitAction}&restaurantId=${restaurantId}`;
+
+  //   if not work change page type and other think like type
+  const url = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}&type=${type}&offset=0&page_type=${page_type}&collection=${collection}`;
+  await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("An error occurred");
+    });
+});
+
+app.get("/", (req, res) => {
+  res.json({ test: "Welcome to Swiggy api!" });
+});
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
